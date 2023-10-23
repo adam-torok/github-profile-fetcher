a430e5aa7abe9e7103baf3a461f57b9e0c653ed3
Török Ádám <adam-torok@outlook.hu>
2023-10-23 21:31:39 +0200

    Adding .env, more dependencies

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,18 +1,55 @@
+import { useState } from "react";
+import { DebounceInput } from "react-debounce-input";
+
 export default function Input() {
-    const classNames = 'border-2 border-black outline-non  block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm placeholder:text-gray-400  sm:text-sm sm:leading-6'
+    const classNames =
+        "border-2 border-black outline-non block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
+
+    const [search, setSearch] = useState("")
+    const [error, setError] = useState("")
+    const AT = import.meta.env.VITE_GITHUB_ACCESS_TOKEN
+
+    const handleFetchFromGithub = (searchText) => {
+        fetch(`https://api.github.com/users/${searchText}`, {
+            headers: {
+                'Authorization': `Bearer ${AT}`
+            }
+        })
+            .then((resp) => {
+                if (!resp.ok) {
+                    throw new Error(resp.statusText)
+                }
+                setError("");
+                return resp.json()
+            })
+            .then((data) => {
+                setError("")
+            })
+            .catch((err) => {
+                setError(err.message)
+            });
+    };
+
+    const handleInputChange = (e) => {
+        const searchText = e.target.value
+        setSearch(searchText)
+        handleFetchFromGithub(searchText)
+    };
+
     return (
         <>
-            <input
-                type="text"
+            {error && <p>{error}</p>}
+            <DebounceInput
+                minLength={2}
+                className={classNames}
                 placeholder="Username of wanted Github user"
                 name="username"
+                type="search"
                 id="username"
-                className={classNames}
-            />
+                debounceTimeout={500}
+                onChange={(e) => handleInputChange(e)} />
 
-            <small>
-                Type the wanted github user's username to the input field to show a basic statistic.
-            </small>
+            <small>Type the wanted github user's username to the input field to show a basic statistic.</small>
         </>
-    )
+    );
 }
