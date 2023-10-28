3aed1de746d4af27e35f76e465a548426be331ac
Török Ádám <adam-torok@outlook.hu>
2023-10-28 17:56:07 +0200

    Adding masonry design for repositories
    Also added some nice animations for the repo cards

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -3,29 +3,44 @@ import { useEffect } from "react"
 import { fetchRepos } from "../redux/repos"
 import { useParams } from "react-router-dom"
 import RepoCard from "../components/cards/RepoCard"
+import Masonry from 'react-masonry-css'
+import { motion as m } from "framer-motion"
 
 export default function Show() {
     const repos = useSelector((state) => state.repos.repos)
     const params = useParams()
     const dispatch = useDispatch()
 
+    const breakpointColumnsObj = {
+        default: 4,
+        1100: 3,
+        700: 2,
+        500: 1
+    }
+
     useEffect(() => {
         dispatch(fetchRepos(params.username))
     }, [])
 
+    const repositories = repos.map((repo) => {
+        return (
+            <m.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                whileHover={{ scale: 1.1 }}
+                key={repo.id}>
+                <RepoCard repo={repo} />
+            </m.div>)
+    })
+
     return (
-        <div className="grid grid-cols-4 gap-4 container mx-auto mt-5 max-w-7xl">
-            {repos ? (
-                <>
-                    {repos.map((repo) => (
-                        <div key={repo.id}>
-                            <RepoCard repo={repo} />
-                        </div>
-                    ))}
-                </>
-            ) : (
-                <div className="dark:text-white"> No repositories found.</div>
-            )}
+        <div className="max-w-7xl mx-auto mt-10">
+            <Masonry
+                breakpointCols={breakpointColumnsObj}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column">
+                {repositories}
+            </Masonry>
         </div>
     )
 
